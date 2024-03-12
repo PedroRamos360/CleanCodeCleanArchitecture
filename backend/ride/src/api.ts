bd2107dd72bef187be37059768d9fd334efcde37
Pedro Ramos <pedrowarkenramos@gmail.com>
2024-03-12 07:45:49 -0300

    starting to implement queues to decouple the microservices

--- backend/ride/src/api.ts
+++ backend/ride/src/api.ts
@@ -2,12 +2,16 @@ import { AcceptRide } from "./application/usecase/AcceptRide";
 import { FinishRide } from "./application/usecase/FinishRide";
 import GetRide from "./application/usecase/GetRide";
 import { RequestRide } from "./application/usecase/RequestRide";
+import { SendReceipt } from "./application/usecase/SendReceipt";
 import { StartRide } from "./application/usecase/StartRide";
 import { UpdatePosition } from "./application/usecase/UpdatePosition";
 import { getEnviormentVariable } from "./env/getEnvironmentVariable";
 import { MainController } from "./infra/controller/MainController";
 import { PgPromiseAdapter } from "./infra/database/PgPromiseAdapter";
+import Registry from "./infra/di/Registry";
 import { ExpressAdapter } from "./infra/http/ExpressAdapter";
+import Queue from "./infra/queue/Queue";
+import QueueController from "./infra/queue/QueueController";
 import { AccountRepositoryApi } from "./infra/repository/AccountRepositoryApi";
 import { PositionRepositoryDatabase } from "./infra/repository/PositionRepositoryDatabase";
 import { RideRepositoryDatabase } from "./infra/repository/RideRepositoryDatabase";
@@ -33,6 +37,13 @@ const acceptRide = new AcceptRide(rideRepository, accountRepository);
 const finishRide = new FinishRide(rideRepository);
 const startRide = new StartRide(rideRepository);
 const updatePosition = new UpdatePosition(positionRepository, rideRepository);
+const sendReceipt = new SendReceipt();
+
+// registry
+const registry = Registry.getInstance();
+registry.register("requestRide", requestRide);
+registry.register("queue", new Queue());
+registry.register("sendReceipt", sendReceipt);
 
 // interface adapter
 new MainController(
@@ -44,4 +55,5 @@ new MainController(
   startRide,
   updatePosition
 );
+new QueueController();
 httpServer.listen(Number(getEnviormentVariable("RIDE_PORT")));
--- backend/ride/src/application/usecase/FinishRide.ts
+++ backend/ride/src/application/usecase/FinishRide.ts
@@ -1,8 +1,9 @@
 import { Ride } from "../../domain/Ride";
+import Queue from "../../infra/queue/Queue";
 import { RideRepository } from "../repository/RideRepository";
 
 export class FinishRide {
-  constructor(private rideRepository: RideRepository) {}
+  constructor(private rideRepository: RideRepository, private queue: Queue) {}
 
   async execute(rideId: string): Promise<Ride> {
     const ride = await this.rideRepository.getById(rideId);
@@ -11,6 +12,10 @@ export class FinishRide {
       throw new Error("Ride is not in progress");
     ride.finish();
     await this.rideRepository.update(ride);
+    await this.queue.publish("rideCompleted", {
+      rideId,
+      amount: ride.getFare(),
+    });
     return ride;
   }
 }
